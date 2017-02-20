d41a749c6dd5e66b0ca82c615c7030b515ca5b5f
L-Elyse <lguimont@student.rcc.edu>
2017-02-19 20:31:25 -0800

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Homework/Gaddis_8thEd_Chapter3_Problem12_CelsiustoFahrenheit/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File: main.cpp
+ * Author: Laurie Guimont
+ * Created: February 19, 2017, 8:24 PM
+ * Purpose: Convert Celsius to Fahrenheit
+ */
+
+//System Libraries
+#include <cstdlib> //Input/Output Stream Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process Data
+    
+    //Output Data
+    
+    //Exit Stage Right!
+    return 0;
+}
+
