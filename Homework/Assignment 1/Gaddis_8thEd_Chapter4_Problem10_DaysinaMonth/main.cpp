6211f66c03a3b9581434ce87acbb6823594badb0
L-Elyse <lguimont@student.rcc.edu>
2017-02-21 13:20:31 -0800

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Homework/Assignment 1/Gaddis_8thEd_Chapter4_Problem10_DaysinaMonth/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File: main.cpp
+ * Author: Laurie Guimont
+ * Created: February 21, 2017,  PM
+ * Purpose: Days in a Month
+ */
+
+//System Libraries
+#include <iostream> //Input/Output Stream Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process Data
+    
+    //Output Data
+    
+    //Exit Stage Right!
+    return 0;
+}
+
