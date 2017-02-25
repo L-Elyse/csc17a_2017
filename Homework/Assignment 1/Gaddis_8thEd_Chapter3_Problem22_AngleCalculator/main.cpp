ab627432cf1b8893c52117922210f5d48600f6b4
L-Elyse <lguimont@student.rcc.edu>
2017-02-25 01:08:47 -0800

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 1/Gaddis_8thEd_Chapter3_Problem22_AngleCalculator/main.cpp
+++ Homework/Assignment 1/Gaddis_8thEd_Chapter3_Problem22_AngleCalculator/main.cpp
@@ -20,17 +20,33 @@ using namespace std;  //Iostream uses the standard namespace
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
-    float angle;
+    float angle,numratr,denom;
+    unsigned int choice;
     
     //Input data
     cout<<"I love math. You should too. Let's have some fun!"<<endl;
-    cout<<"Enter an angle (in rads) and I will show you its Sine, Cosine";
-    cout<<" and Tangent\n";
-    cout<<"(For pi, please enter 3.14159)"<<endl;
-    cin>>angle;
+    cout<<"If you enter an angle (in rads), I will show you its Sine, Cosine ";
+    cout<<"and Tangent\n";
+    cout<<"First, please choose what type of number you would like to input\n";
+    cout<<"Type '1' to input a whole number or decimal"<<endl;
+    cout<<"Type '2' to input a fraction"<<endl;
+    cin>>choice;
     
     //Process data
-    
+    if(choice==1){
+        cout<<"What number would you like information on?"<<endl;
+        cin>>angle;
+    }else if (choice==2){
+        cout<<"What is the numerator?\n";
+        cin>>numratr;
+        cout<<"What is the denominator?\n";
+        cin>>denom;
+        angle=numratr/denom;
+    }else{
+        cout<<"Clearly you're not serious about this. Bye."<<endl;
+        return 0; //THIS IS AN ISSUE!!!
+    }
+        
     //Output data
     cout<<fixed<<setprecision(4)<<showpoint<<endl;
     cout<<"Sine = "<<sin(angle)<<endl;
--- Homework/Assignment 1/Gaddis_8thEd_Chapter4_Problem10_DaysinaMonth/main.cpp
+++ Homework/Assignment 1/Gaddis_8thEd_Chapter4_Problem10_DaysinaMonth/main.cpp
@@ -1,12 +1,12 @@
 /* 
  * File: main.cpp
  * Author: Laurie Guimont
- * Created: February 21, 2017,  PM
+ * Created: February 24, 2017, 8:30 PM
  * Purpose: Days in a Month
  */
 
 //System Libraries
-#include <iostream> //Input/Output Stream Library
+#include <iostream>  //Input/Output Stream Library
 using namespace std;
 
 //User Libraries
@@ -18,12 +18,44 @@ using namespace std;
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
+    unsigned int month, year;
     
     //Input Data
+    cout<<"Enter a month (1-12): ";
+    cin>>month;
+    cout<<"Enter a year: ";
+    cin>>year;
+    cout<<endl;
     
-    //Process Data
-    
-    //Output Data
+    //Process & Output Data
+    switch(month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: 
+            cout<<"31 days"<<endl;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            cout<<"30 days"<<endl;
+            break;
+        case 2:
+            if(year%100==0){
+                if(year%400==0)
+                    cout<<"29 days"<<endl;
+            }else if(year%4==0)
+                cout<<"29 days"<<endl;
+            else
+                cout<<"28 days"<<endl;
+            break;
+        default:
+            cout<<"Not a valid entry. Sorry"<<endl;        
+    }
     
     //Exit Stage Right!
     return 0;
