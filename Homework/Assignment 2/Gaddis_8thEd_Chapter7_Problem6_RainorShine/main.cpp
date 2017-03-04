b35df5d64915d4eb3841496c0313a895711fa7f9
L-Elyse <lguimont@student.rcc.edu>
2017-03-04 01:14:58 -0800

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Homework/Assignment 2/Gaddis_8thEd_Chapter7_Problem6_RainorShine/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on March 4, 2017, 12:27 PM
+ * Purpose: Rain or Shine Program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables, no doubles
+    
+    //Input data
+    
+    //Process data
+    
+    //Output data
+    
+    //Exit Stage Right!
+    return 0;
+}
