7607befc39da2c6302f57f8dbbd00a26cebb54ef
L-Elyse <lguimont@student.rcc.edu>
2017-03-05 16:24:00 -0800

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 2/Gaddis_8thEd_Chapter7_Problem6_RainorShine/main.cpp
+++ Homework/Assignment 2/Gaddis_8thEd_Chapter7_Problem6_RainorShine/main.cpp
@@ -7,24 +7,98 @@
 
 //System Libraries
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-//User Libraries
-
 //Global Constants
+const int COL=30;     //Columns for 2-Dimensional Array
 
 //Function Prototypes
+void readin(string [],char [][COL], int);
+void process(char [][COL], int);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
-    
-    //Input data
-    
-    //Process data
-    
-    //Output data
+    const int ROW=3;
+    char rcs[ROW][COL];
+ 
+    //Process & Output the Data
+    process(rcs,ROW);
     
     //Exit Stage Right!
     return 0;
+}
+
+void process(char rcs [][COL],int r){
+    //Declare Variables
+    int rain=0,cloud=0,sun=0;      //Overall rain, cloud, or sun over period
+    int juner=0,julyr=0,augustr=0; //total rain per month
+    int junec=0,julyc=0,augustc=0; //total cloud per month
+    int junes=0,julys=0,augusts=0; //total sun per month
+    string months[r]={"June  ", "July  ", "August"};
+    
+    //Call Function to Display Array
+    readin(months,rcs,r);
+    
+    //Process the Data
+    for(int i=0;i<r;i++){
+        for(int j=0;j<COL;j++){
+            if(rcs[i][j]=='R'){
+                rain++;
+                if(i==0) juner++;
+                else if(i==1) julyr++;
+                else augustr++;
+            }
+            if(rcs[i][j]=='C'){
+                cloud++;
+                if(i==0) junec++;
+                else if(i==1) julyc++;
+                else augustc++;
+            }
+            if(rcs[i][j]=='S'){
+                sun++;
+                if(i==0) junes++;
+                else if(i==1) julys++;
+                else augusts++;
+            }
+        }
+    }
+    
+    //Display Stats
+    cout<<endl<<"\tRain\t Cloud\t  Sun"<<endl;
+    cout<<"        ---------------------"<<endl;
+    cout<<"June:    "<<juner<<"\t  "<<junec<<"\t  "<<junes<<endl;
+    cout<<"July:    "<<julyr<<"\t  "<<julyc<<"\t  "<<julys<<endl;
+    cout<<"August:  "<<augustr<<"\t  "<<augustc<<"\t  "<<augusts<<endl<<endl;
+    cout<<"Total number of rainy days over three months:   "<<rain<<"\n";
+    cout<<"Total number of cloudy days over three months:  "<<cloud<<"\n";
+    cout<<"Total number of sunny days over three months:   "<<sun<<"\n";
+    
+    //Exit Function
+    return;
+}
+
+void readin(string months[],char rcs[][COL], int r){
+    //Declare the File
+    ifstream in;
+    
+    //Open the Input File
+    in.open("RainorShine.txt");
+    
+    //Read In the File
+    for(int i=0;i<r;i++){
+        cout<<months[i]<<":  ";
+        for(int j=0;j<COL;j++){
+            in>>rcs[i][j];
+            cout<<rcs[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    //Close the Input File
+    in.close();
+    
+    //Exit Function
+    return;
 }
---
+++ Homework/Assignment 2/Gaddis_8thEd_Chapter8_Problem7_BinaryStringSearch/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on March 5, 2017, 4:20 PM
+ * Purpose: 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables, no doubles
+    
+    //Input data
+    
+    //Process data
+    
+    //Output data
+    
+    //Exit Stage Right!
+    return 0;
+}
