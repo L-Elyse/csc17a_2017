63205e3acefd2b294272598c73ca60212ccee5a9
L-Elyse <lguimont@student.rcc.edu>
2017-03-20 15:39:01 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Lab/Dynamic Memory Database Sort_Pointers/main.cpp
@@ -0,0 +1,88 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on March 20th, 2017, 3:30 PM
+ * Purpose: Database Sort using only Pointer Notation
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+void fillAry(int *,int *,int,int);
+void prntAry(int *,int,int);
+void prntAry(int *,int *,int,int);
+void markSrt(int *,int*,int);
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables, no doubles
+    int SIZE=100;
+    int *array, *indx;
+    array=new int[SIZE];
+    indx=new int[SIZE];
+    fillAry(array,indx,SIZE,10);
+    
+    //Output the created array
+    prntAry(array,SIZE,10);
+    prntAry(indx,SIZE,10);
+    
+    //Sort the Array
+    markSrt(array,indx,SIZE);
+    
+    //Print our the Sorted Array
+    prntAry(array,SIZE,10);
+    prntAry(indx,SIZE,10);
+    prntAry(array,indx,SIZE,10);
+    
+    //Delete the Arrays
+    delete []array;
+    delete []indx;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
+void prntAry(int *a,int *indx,int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<(*(a+*(indx+i))<10?"  ":" ")<<*(a+*(indx+i));
+        if(i%perLine==(perLine-1))
+            cout<<endl;
+    }
+    cout<<endl;
+}
+
+void markSrt(int *a,int *indx,int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(*(a+*(indx+i))>*(a+*(indx+j))){
+                int temp=*(indx+i);
+                *(indx+i)=*(indx+j);
+                *(indx+j)=temp;
+            }
+        }
+    }
+}
+
+void prntAry(int *a,int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<(*(a+i)<10?"  ":" ")<<*(a+i);
+        if(i%perLine==(perLine-1))
+            cout<<endl;
+    }
+    cout<<endl;
+}
+
+void fillAry(int *a,int *indx,int n,int mod){
+    for(int i=0;i<n;i++){
+        *(a+i)=i%mod;
+        *(indx+i)=i;
+    }
+}
--- Lab/Dynamic Memory Database Sort_Pointers/nbproject/Makefile-Release.mk
+++ Lab/Dynamic Memory Database Sort_Pointers/nbproject/Makefile-Release.mk
@@ -56,11 +56,11 @@ LDLIBSOPTIONS=
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
-	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort.exe
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort_pointers.exe
 
-${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort.exe: ${OBJECTFILES}
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort_pointers.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort ${OBJECTFILES} ${LDLIBSOPTIONS}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort_pointers ${OBJECTFILES} ${LDLIBSOPTIONS}
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
@@ -73,7 +73,7 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort.exe
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/dynamic_memory_database_sort_pointers.exe
 
 # Subprojects
 .clean-subprojects:
