7ce997904e3e26846888d19c40a6316da4a1d2ae
L-Elyse <lguimont@student.rcc.edu>
2017-04-04 21:39:09 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Homework/Assignment 4/Gaddis_8thEd_Chapter10_Problem2_BackwardsString/nbproject/Makefile-Release.mk
+++ Homework/Assignment 4/Gaddis_8thEd_Chapter10_Problem2_BackwardsString/nbproject/Makefile-Release.mk
@@ -62,7 +62,7 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gaddis_8thed_chapter10_problem2_backw
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gaddis_8thed_chapter10_problem2_backwardsstring ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.cpp 
+${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
@@ -73,7 +73,6 @@ ${OBJECTDIR}/main.o: main.cpp
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gaddis_8thed_chapter10_problem2_backwardsstring.exe
 
 # Subprojects
 .clean-subprojects:
--- Homework/Assignment 4/Gaddis_8thEd_Chapter10_Problem7_NameArranger/main.cpp
+++ Homework/Assignment 4/Gaddis_8thEd_Chapter10_Problem7_NameArranger/main.cpp
@@ -15,26 +15,49 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void arrange(char []);
+void arrange(char [],int);
+
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare variables, no doubles
     int size=100;
     char name[size];
+    int length;
     
     //Get the String from User
     cout<<"Enter your first, middle, and last name."<<endl;
     cin.getline(name,size);
+    length=strlen(name);
     
     //Output the String Length
     cout<<endl;
-    cout<<"Here is your name rearranged: "<<arrange(name)<<endl;
+    cout<<"Here is your name rearranged: ";
+    arrange(name,length);
     
     //Exit Stage Right!
     return 0;
 }
 
-void arrange(char c[]){
-    
+void arrange(char c[],int l){
+    char first[l],middle[l],last[l];
+    int count=1,index=0;
+    for(int i=0;i<l;i++){
+        if(c[i]!=' '){
+            if(count==1)
+                first[index]=c[i];
+            else if(count==2)
+                middle[index]=c[i];
+            else if(count==3)
+                last[index]=c[i]; 
+            index++;
+        }
+        else{
+            count++;
+            index=0;
+        }
+    }
+    //Now Arrange into One Array
+    int size=strlen(first)+strlen(middle)+strlen(last)+1;
+    char combine[size];
 }
