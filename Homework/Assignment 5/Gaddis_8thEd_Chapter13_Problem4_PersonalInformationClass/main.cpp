aec95092422c27d6509e91365ef560ab665b0f50
L-Elyse <lguimont@student.rcc.edu>
2017-05-08 02:24:57 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

---
+++ Homework/Assignment 5/Gaddis_8thEd_Chapter13_Problem4_PersonalInformationClass/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   main.cpp
+ * Author: Laurie Guimont
+ * Created on May 8, 2017, 1:23 AM
+ * Purpose: Problem 4
+ */
+
+//System Libraries
+#include <iostream>
+#include <string>
+using namespace std;
+
+//User Libraries
+#include "Personal.h"
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    Personal people[3];
+    string n,a,p;
+    int g;
+    
+    //Fill Array
+    cout<<"Enter the following information for yourself and then two others\n";
+    for(int i=0;i<3;i++){
+        cout<<"Person "<<i+1<<": "<<endl;
+        cout<<"Name: ";
+        getline(cin,n);
+        cout<<"Address: ";
+        getline(cin,a);
+        cout<<"Age: ";
+        cin>>g;
+        cin.ignore();
+        cout<<"Phone: ";
+        getline(cin,p);
+        
+        cout<<"Here are what you input: \n";
+        //Set the Information
+        people[i].setname(n);
+        people[i].setaddy(a);
+        people[i].setage(g);
+        people[i].setphne(p);
+        
+        //Get the Information
+        cout<<"Person "<<i+1<<endl;
+        cout<<"Name: "<<people[i].getname()<<endl;
+        cout<<"Address: "<<people[i].getaddy()<<endl;
+        cout<<"Age: "<<people[i].getage()<<endl;
+        cout<<"Phone: "<<people[i].getphne()<<endl;
+    }
+
+    //Exit Stage Right!
+    return 0;
+}
