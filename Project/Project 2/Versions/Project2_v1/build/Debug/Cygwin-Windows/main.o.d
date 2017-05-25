88bac48b0014ee9c2999a1365c070614389beb31
L-Elyse <lguimont@student.rcc.edu>
2017-05-25 16:17:42 -0700

    Signed-off-by: L-Elyse <lguimont@student.rcc.edu>

--- Project/Project 2/Versions/Project2_v1/build/Debug/Cygwin-Windows/main.o.d
+++
@@ -1,7 +0,0 @@
-build/Debug/Cygwin-Windows/main.o: main.cpp Die.h Board.h Property.h
-
-Die.h:
-
-Board.h:
-
-Property.h:
--- Project/Project 2/Versions/Project2_v2/build/Debug/Cygwin-Windows/Property.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/Property.o: Property.cpp Property.h Board.h
-
-Property.h:
-
-Board.h:
